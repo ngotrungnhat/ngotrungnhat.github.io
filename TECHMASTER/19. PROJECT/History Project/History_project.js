ed26a75d912daaf62384087c9993580427468226
Nhất Nhất Nhất <ngonhat95@gmail.com>
2018-10-02 10:29:56 +0700

    Update_Tuesday, October 2, 2018

--- TECHMASTER/19. PROJECT/History Project/History_project.js
+++ TECHMASTER/19. PROJECT/History Project/History_project.js
@@ -2,40 +2,21 @@
 //RESIZE CHIỀU CAO CỦA FIXED THEO FLAG.
 $(".fixedFlag").height($(".flag").height());
 
-
-// console.log(scaleOpacity.toFixed(2));
-
-function opacity(){
-
-	var arrScale = [];
-	var heightScroll = $("html").scrollTop();
-	var heightScreen = screen.availHeight;
-	var scaleOpacity = heightScroll/heightScreen;
-
-	for (i = 2; i >= 0; i = i-0.2) {
-		// console.log(i.toFixed(2));
-		arrScale.push(i.toFixed(2));
-		console.log(arrScale);
-	}
-
-	for (var j = 0; j <= 1; j = j + 0.1) {
-		// console.log(j.toFixed(1));
-		if (scaleOpacity.toFixed(2) >= arrScale[j.toFixed(1)*10] && scaleOpacity.toFixed(2) <= arrScale[j.toFixed(1)*10 +1]) {
-			console.log("j: " + j.toFixed(1));
-			// $(".flag").css("opacity","j.toFixed(1)");
-		}
-		console.log("arrScale[j*10]: " + arrScale[j.toFixed(1)*10]);
-		console.log("scaleOpacity: " + scaleOpacity.toFixed(2));
-		console.log("arrScale[j*10+1]: " + arrScale[j.toFixed(1)*10 + 1]);
-	}
-}
-opacity()
-// function myInterval(){
-// 	interval = setInterval(opacity, 1000);
+//OPACITY FLAG
+// function opacity(){
+// 	var arrScale = [];
+// 	var heightScroll = $("html").scrollTop();
+// 	var heightScreen = screen.availHeight;
+// 	var scaleOpacity = heightScroll/heightScreen;
+// 	for (i = 2; i >= 0; i = i-0.2) {
+// 		arrScale.push(i.toFixed(2));
+// 	}
+// 	for (var j = 0; j <= 1; j = j + 0.1) {
+// 		if (scaleOpacity.toFixed(2) >= arrScale[j.toFixed(1)*10] && scaleOpacity.toFixed(2) <= arrScale[j.toFixed(1)*10 +1]) {
+// 		}
+// 	}
 // }
-// myInterval();
-
-// $(".flag").css("opacity","0.5");
+// opacity()
 
 //TOGGLE WHEN SMALL SCREEN
 function navbarToggle(x){
@@ -64,6 +45,16 @@ $(document).ready(function(){
 		$(this).css({"color": "#753721", "border": "none"});
 	});
 });
+
+// Get the modal
+var modal = document.getElementById('story-01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 
