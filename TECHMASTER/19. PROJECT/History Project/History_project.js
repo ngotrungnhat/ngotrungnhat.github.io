38417600ef628da2a06e85be16ee648fc3fab68a
Nhất Nhất Nhất <ngonhat95@gmail.com>
2018-10-17 23:55:34 +0700

    Update

--- TECHMASTER/19. PROJECT/History Project/History_project.js
+++ TECHMASTER/19. PROJECT/History Project/History_project.js
@@ -31,10 +31,10 @@ function navbarToggle(x){
 
 //EFECT HOVER
 // $(document).ready(function(){
-// 	$("div.promo").hover(function(){
-// 		$(this).css({"color": "#ca27b1", "cursor": "pointer"});
+// 	$(".pageList li:nth-child(n)").hover(function(){
+// 		$(".grid4__col div.cards:nth-child(n)").css({"color": "#ca27b1", "cursor": "pointer", "border": "2px solid #ca27b1"});
 // 	}, function(){
-// 		$(this).css("color", "#753721");
+// 		$(".grid4__col div.cards:nth-child(n)").css({"color": "#753721", "border": "none"});
 // 	});
 // });
 
@@ -46,6 +46,7 @@ function navbarToggle(x){
 // 	});
 // });
 
+
 var modal = document.getElementById('story-01');
 window.onclick = function(event) {
     if (event.target == modal) {
@@ -53,13 +54,53 @@ window.onclick = function(event) {
     }
 }
 
+$(document).ready(function(){
+	let index;
+	const lis = $('.pageList li');
+	const cards = $('.cards');
+	lis.hover(function(){
+		index = $(this).attr('data-test');
+		const cardIndex = $(`.cards[data-hover='${index}']`);
+		$(cardIndex).css({"color": "#ca27b1", "cursor": "pointer", "border": "2px solid #ca27b1"});
+	}, function(){
+		cards.css({"color": "#753721", "border": "none"});
+	});
+});
 
 
 
 
+// let html = `<div class="containerFoot">
+// 								<div class="foot footRight" data-aos="fade-zoom-in" data-aos-duration="2000" data-aos-offset="300"><img src="Image_history/footprint_right.png" alt=""></div>
+// 								<div class="foot footLeft" data-aos="fade-zoom-in" data-aos-duration="3000" data-aos-offset="300"><img src="Image_history/footprint_left.png" alt=""></div>
+// 							</div>`;
+
+// for (let i = 0; i< 10; i++){
+// 	$('.contentBetween').append(html);
+// }
 
 
 
+
+//BACK TO TOP
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1500) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},1500);
+    });
+});
+
+
+$(document).ready(function(){
+	$('html,body').animate({},10000)
+});
 
 
 
