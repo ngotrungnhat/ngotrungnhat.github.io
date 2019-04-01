eb8a968be648292acd40fc3ffa8dd76f41e94524
Nhất Nhất Nhất <ngonhat95@gmail.com>
2019-04-01 17:47:45 +0700

    1/4/2019

--- STUDYING/react-frontend-for-backend/react-nodejs/frontend/product_frontend/src/Components/Product.js
+++ STUDYING/react-frontend-for-backend/react-nodejs/frontend/product_frontend/src/Components/Product.js
@@ -8,8 +8,17 @@ export default class Product extends Component {
           <img className="card-img-top" src={this.props.image} alt="true" />
           <div className="card-body">
             <h4 className="card-title">{this.props.product_name}</h4>
-            <p className="card-text">{this.props.product_price}</p><p>
-            </p></div>
+            <p className="card-text">{this.props.product_price}</p>
+          </div>
+          <div className="btn-group" style={{width: '100%'}}>
+              <div className="btn btn-warning sua">
+                  <i className="fa fa-edit">Sửa</i>
+              </div>
+              <div className="btn btn-danger xoa">
+                  <i className="fa fa-delete">Xóa</i>
+              </div>
+          </div>
+
         </div>
       </div>
     )
